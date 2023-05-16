46b347b5f7b80c7087cd1dd42b741abb39e201f7
Esdras7 <souzaesdras@alu.ufc.br>
2023-05-16 17:58:13 -0300

    cansaço

--- ModoEscuro.js
+++ ModoEscuro.js
@@ -1,7 +1,36 @@
 const btn = document.getElementById("tema");
 const lnk = document.querySelector("link[href='../style.css']");
 const stlcss = new URL("../style.css", window.location)
+const thema = document.querySelector('.js-sair')
 
+
+console.log(thema)
+Cookies.set("Nome", "Esdras")
+
+thema.addEventListener("click", function () {
+    if (btn.checked == true) {
+        Cookies.set('dark', true)
+        console.log("True")
+    }
+    else {
+        Cookies.set('dark', false)
+        console.log("True")
+    }
+})
+
+
+//Vindo de outro parte do site
+if (Cookies.get('dark')) {
+    function ativarCheckbox(el) {
+        el.checked = true;
+    }
+    ativarCheckbox(btn);
+    lnk.href = "../ModoEscuro.css";
+}
+
+
+
+//Quando Clicar no Botão
 btn.addEventListener("click", function () {
     if (lnk.href == stlcss.href) {
         lnk.href = "../ModoEscuro.css";
